404139121bb130f733e068b8427768774226a7c1
Mohamed ElDarwish <Mohamed.Eldarwish@originsysglobal.com>
2024-11-22 21:05:48 +0200

    edit audit

--- app/Http/Controllers/AuditController.php
+++ app/Http/Controllers/AuditController.php
@@ -10,11 +10,6 @@ class AuditController extends Controller
     public function index()
     {
         $audits = Audit::with('user')->latest()->get();
-
-        // // Optionally format the modified data
-        // foreach ($audits as $audit) {
-        //     $audit->modified = $audit->getModified();
-        // }
         return view('audits.index', compact('audits'));
     }
 }
--- resources/views/audits/index.blade.php
+++ resources/views/audits/index.blade.php
@@ -8,24 +8,28 @@
             <tr>
                 <th>Event</th>
                 <th>User</th>
-                <th>Attribute</th>
-                <th>Old Value</th>
-                <th>New Value</th>
+                <th>Changes</th>
                 <th>Timestamp</th>
             </tr>
         </thead>
         <tbody>
             @foreach($audits as $audit)
-            @foreach($audit->getModified() as $attribute => $modified)
             <tr>
                 <td>{{ $audit->event }}</td>
                 <td>{{ $audit->user->name ?? 'System' }}</td>
-                <td>{{ $attribute }}</td>
-                <td style="color: red;">{{ isset($modified['old']) ? json_encode($modified['old']) : 'N/A' }}</td>
-                <td style="color: green;">{{ isset($modified['new']) ? json_encode($modified['new']) : 'N/A' }}</td>
+                <td>
+                    <ul>
+                        @foreach($audit->getModified() as $attribute => $modified)
+                        <li>
+                            <strong>{{ $attribute }}</strong>: 
+                            <span style="color: red;">{{ isset($modified['old']) ? json_encode($modified['old']) : 'N/A' }}</span> → 
+                            <span style="color: green;">{{ isset($modified['new']) ? json_encode($modified['new']) : 'N/A' }}</span>
+                        </li>
+                        @endforeach
+                    </ul>
+                </td>
                 <td>{{ $audit->created_at }}</td>
             </tr>
-            @endforeach
             @endforeach
         </tbody>
     </table>
